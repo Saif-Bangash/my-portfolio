9e74f92da3cb2e1b563aceae86f7095588f55c5f
Saif-Bangash <saifbangash480@gmail.com>
2025-05-13 22:50:21 +0500

    Added profile image and updated component

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,17 +1,21 @@
+import profileLogo from "../assets/profile-logo.jpg";
+
 export default function About() {
   return (
-    <section className="bg-gray-900 text-white py-33 px-6 md:px-16 lg:px-32" id="about">
+    <section
+      className="bg-gray-900 text-white py-33 px-6 md:px-16 lg:px-32"
+      id="about"
+    >
       <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-10">
-        
         {/* Left Side Content */}
         <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-4xl font-bold mb-4">
             About <span className="text-cyan-400">Me</span>
           </h2>
           <p className="text-gray-400 mb-6">
-            I'm a   Frontend Developer with one year experience in building
-            modern and responsive web applications. I specialize in React,
-            Tailwind CSS, and creating intuitive UI/UX designs.
+            I'm a Frontend Developer with one year experience in building modern
+            and responsive web applications. I specialize in React, Tailwind
+            CSS, and creating intuitive UI/UX designs.
           </p>
           <p className="text-gray-400 mb-6">
             I am always eager to learn new technologies and take on challenging
@@ -34,15 +38,17 @@ export default function About() {
             {/* Profile Image with Hexagonal Shape */}
             <div className="relative w-60 h-60 bg-gray-800 rounded-lg flex items-center justify-center">
               <img
-                src="src/assets/profile-logo.jpg" // Make sure image is placed in `public/assets/`
+                src={profileLogo}
                 alt="Profile"
                 className="w-full h-full object-cover rounded-lg"
-                style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }}
+                style={{
+                  clipPath:
+                    "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
+                }}
               />
             </div>
           </div>
         </div>
-
       </div>
     </section>
   );
